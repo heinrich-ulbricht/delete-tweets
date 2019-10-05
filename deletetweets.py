d2543129a81ecbf02116879a81145848a2ec2e66
Heinrich Ulbricht <github@heinrich-ulbricht.de>
2019-10-06 00:13:58 +0200

    adding "live mode" to get tweets from the user timeline #55 adding "--remove-likes" switch to remove likes as well (currently only in "live mode") #8 adding tweet text to console output to make watching it less boring refactoring some variable names to make their meaning clearer
    https://github.com/koenrh/delete-tweets/issues/55
    https://github.com/koenrh/delete-tweets/issues/8

--- deletetweets.py
+++ deletetweets.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 import io
@@ -10,7 +10,7 @@ import json
 import twitter
 from dateutil.parser import parse
 
-__author__ = "Koen Rouwhorst"
+__author__ = "Koen Rouwhorst, Heinrich Ulbricht"
 __version__ = "1.0.0"
 
 
@@ -18,67 +18,154 @@ class TweetDestroyer(object):
     def __init__(self, twitter_api):
         self.twitter_api = twitter_api
 
-    def destroy(self, tweet_id):
+    def destroy(self, id_str, created_at, full_text):
         try:
-            print("delete tweet %s" % tweet_id)
-            self.twitter_api.DestroyStatus(tweet_id)
+            print("delete tweet %s (%s: '%s')" % (id_str, created_at, full_text))
+            self.twitter_api.DestroyStatus(id_str)
             # NOTE: A poor man's solution to honor Twitter's rate limits.
             time.sleep(0.5)
         except twitter.TwitterError as err:
             print("Exception: %s\n" % err.message)
 
+class LikeRemover(object):
+    def __init__(self, twitter_api):
+        self.twitter_api = twitter_api
+
+    def removeLike(self, id_str, created_at, full_text):
+        try:
+            print("removing like %s (%s: '%s')" % (id_str, created_at, full_text))
+
+            self.twitter_api.DestroyFavorite(status_id=id_str)
+            # NOTE: A poor man's solution to honor Twitter's rate limits.
+            time.sleep(0.5)
+        except twitter.TwitterError as err:
+            print("Exception: %s\n" % err.message)
 
 class TweetReader(object):
-    def __init__(self, reader, date=None, restrict=None, spare=[], min_likes=0, min_retweets=0):
+    def __init__(self, reader, date=None, restrict=None, spare=[], min_likes=0, min_retweets=0, remove_likes = False):
         self.reader = reader
         if date is not None:
             self.date = parse(date, ignoretz=True).date()
         self.restrict = restrict
         self.spare = spare
-        self.min_likes = 0 if min_likes is None else min_likes
-        self.min_retweets = 0 if min_retweets is None else min_retweets
+        self.min_likes = min_likes
+        self.min_retweets = min_retweets
+        self.remove_likes = remove_likes
 
-    def read(self):
+    def isTweetToDestroy(self, id_str, created_at, full_text, in_reply_to_user_id_str, favorite_count, retweet_count):
+        if created_at != "":
+            tweet_date = parse(created_at, ignoretz=True).date()
+            if self.date != "" and \
+                    self.date is not None and \
+                    tweet_date >= self.date:
+                return False
+
+        if (self.restrict == "retweet" and
+                not full_text.startswith("RT @")) or \
+                (self.restrict == "reply" and
+                    (in_reply_to_user_id_str == "" or in_reply_to_user_id_str == "None")):
+            return False
+
+        if id_str in self.spare:
+            return False
+
+        if (self.min_likes and int(favorite_count) >= self.min_likes) or \
+                (self.min_retweets and int(retweet_count) >= self.min_retweets):
+            return False
+        
+        return True
+
+    def isLikeToRemove(self, id_str, created_at, favorite_count, retweet_count):
+        if created_at != "":
+            tweet_date = parse(created_at, ignoretz=True).date()
+            if self.date != "" and \
+                    self.date is not None and \
+                    tweet_date >= self.date:
+                return False
+
+        if id_str in self.spare:
+            return False
+
+        if (self.min_likes and int(favorite_count) >= self.min_likes) or \
+                (self.min_retweets and int(retweet_count) >= self.min_retweets):
+            return False
+        
+        return True
+
+    def readFromTweetJs(self):
         for row in self.reader:
-            if row.get("created_at", "") != "":
-                tweet_date = parse(row["created_at"], ignoretz=True).date()
-                if self.date != "" and \
-                        self.date is not None and \
-                        tweet_date >= self.date:
-                    continue
-
-            if (self.restrict == "retweet" and
-                    not row.get("full_text").startswith("RT @")) or \
-                    (self.restrict == "reply" and
-                     row.get("in_reply_to_user_id_str") == ""):
-                continue
-
-            if row.get("id_str") in self.spare:
-                continue
-
-            if (self.min_likes > 0 and int(row.get("favorite_count")) >= self.min_likes) or \
-                    (self.min_retweets > 0 and int(row.get("retweet_count")) >= self.min_retweets):
+            if not self.isTweetToDestroy(row.get("id_str"), row.get("created_at", ""), row.get("full_text"), row.get("in_reply_to_user_id_str"), row.get("favorite_count"), row.get("retweet_count")):
                 continue
 
             yield row
 
+    def readUserTimelineLive(self, api):
+        maxCount = 200 # this is the maximum number of statuses retrievable in a single call according to the API documentation
 
-def delete(tweetjs_path, date, r, s, min_l, min_r):
-    with io.open(tweetjs_path, mode="r", encoding="utf-8") as tweetjs_file:
-        count = 0
+        # we'll page with a page size of 200 up to the alledged maximum of 3200 tweets
+        lastId = None
+        while True:
+            if (not lastId):
+                statuses = api.GetUserTimeline(count=maxCount)
+            else :
+                time.sleep(0.5)
+                statuses = api.GetUserTimeline(count=maxCount, max_id=lastId-1)
+                if (len(statuses) == 0):
+                    break
+            for s in statuses:
+                lastId = s.id
+                if not self.isTweetToDestroy(s.id_str, s.created_at, s.text, str(s.in_reply_to_user_id), str(s.favorite_count), str(s.retweet_count)):
+                    continue
+                yield s
 
-        api = twitter.Api(consumer_key=os.environ["TWITTER_CONSUMER_KEY"],
-                          consumer_secret=os.environ["TWITTER_CONSUMER_SECRET"],
-                          access_token_key=os.environ["TWITTER_ACCESS_TOKEN"],
-                          access_token_secret=os.environ["TWITTER_ACCESS_TOKEN_SECRET"])
-        destroyer = TweetDestroyer(api)
+    def readFavoritesLive(self, api):
+        maxCount = 200 # this is the maximum number of statuses retrievable in a single call according to the API documentation
 
-        tweets = json.loads(tweetjs_file.read()[25:])
-        for row in TweetReader(tweets, date, r, s, min_l, min_r).read():
-            destroyer.destroy(row["id_str"])
-            count += 1
+        # we'll page with a page size of 200 up to the alledged maximum of 3200 tweets
+        lastId = None
+        while True:
+            if (not lastId):
+                statuses = api.GetFavorites(count=maxCount)
+            else :
+                time.sleep(0.5)
+                statuses = api.GetFavorites(count=maxCount, max_id=lastId-1)
+                if (len(statuses) == 0):
+                    break
+            for s in statuses:
+                lastId = s.id
+                if not self.isLikeToRemove(s.id_str, s.created_at, str(s.favorite_count), str(s.retweet_count)):
+                    continue
+                yield s
+        
+def getTwitterApi():
+    api = twitter.Api(consumer_key=os.environ["TWITTER_CONSUMER_KEY"],
+                        consumer_secret=os.environ["TWITTER_CONSUMER_SECRET"],
+                        access_token_key=os.environ["TWITTER_ACCESS_TOKEN"],
+                        access_token_secret=os.environ["TWITTER_ACCESS_TOKEN_SECRET"])
+    return api
 
-        print("Number of deleted tweets: %s\n" % count)
+def startProcessing(tweetjs_path, date, restrict, spare_ids, min_likes, min_retweets, remove_likes):
+    api = getTwitterApi()
+    tweetDestroyer = TweetDestroyer(api)
+    likeRemover = LikeRemover(api)
+    deletedTweetCount = 0
+    removedLikeCount = 0
+    if (tweetjs_path != "twitter"):
+        with io.open(tweetjs_path, mode="r", encoding="utf-8") as tweetjs_file:
+            tweets = json.loads(tweetjs_file.read()[25:])
+            for row in TweetReader(tweets, date, restrict, spare_ids, min_likes, min_retweets, remove_likes).readFromTweetJs():
+                tweetDestroyer.destroy(row["id_str"], row["created_at"], row["full_text"])
+                deletedTweetCount += 1
+    else:
+        for status in TweetReader(None, date, restrict, spare_ids, min_likes, min_retweets, remove_likes).readUserTimelineLive(api):
+            tweetDestroyer.destroy(status.id_str, status.created_at, status.text)
+            deletedTweetCount += 1
+        if (remove_likes):
+            for status in TweetReader(None, date, restrict, spare_ids, min_likes, min_retweets, remove_likes).readFavoritesLive(api):
+                likeRemover.removeLike(status.id_str, status.created_at, status.text)
+                removedLikeCount += 1
+            print("Number of removed likes: %s\n" % removedLikeCount)
+    print("Number of deleted tweets: %s\n" % deletedTweetCount)
 
 
 def main():
@@ -87,14 +174,16 @@ def main():
                         help="Delete tweets until this date")
     parser.add_argument("-r", dest="restrict", choices=["reply", "retweet"],
                         help="Restrict to either replies or retweets")
-    parser.add_argument("file", help="Path to the tweet.js file",
+    parser.add_argument("file", help="Path to the tweet.js file -OR- specify 'twitter' to activate LIVE MODE instead: read from current user timeline (Note: there is a limit of 3200 tweets that can be retrieved this way)",
                         type=str)
     parser.add_argument("--spare-ids", dest="spare_ids", help="A list of tweet ids to spare",
                         type=str, nargs="+", default=[])
     parser.add_argument("--spare-min-likes", dest="min_likes",
-                        help="Spare tweets with more than the provided likes", type=int, default=0)
+                        help="Spare tweets with more than the provided likes", type=int)
     parser.add_argument("--spare-min-retweets", dest="min_retweets",
-                        help="Spare tweets with more than the provided retweets", type=int, default=0)
+                        help="Spare tweets with more than the provided retweets", type=int)
+    parser.add_argument("--remove-likes", dest="remove_likes", action="store_true",
+                        help="Remove likes (currently only supported in LIVE MODE, the same 3200 tweet limit applies)")
 
     args = parser.parse_args()
 
@@ -105,7 +194,7 @@ def main():
         sys.stderr.write("Twitter API credentials not set.")
         exit(1)
 
-    delete(args.file, args.date, args.restrict, args.spare_ids, args.min_likes, args.min_retweets)
+    startProcessing(args.file, args.date, args.restrict, args.spare_ids, args.min_likes, args.min_retweets, args.remove_likes)
 
 
 if __name__ == "__main__":
